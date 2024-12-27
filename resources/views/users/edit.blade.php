cbef2f2a6372b7d08f92c1f5681fc84c0f400d82
Daniel Santos <daniel.oliveira@abril.com.br>
2024-12-27 17:31:52 -0300

    ajustes perfil, roles e outros relacionamentos

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,70 +8,18 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 col-12">
-                <form action="{{ route('users.update', $user->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <div class="card card-primary">
-                        <div class="card-header">
-                          <h3 class="card-title">Dados do Usuário</h3>
-                        </div>
-                          <div class="card-body">
-                            <div class="form-group mt-2">
-                                <label for="nome">Nome</label>
-                                <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{ $user->name }}" id="nome" name="name">
-                                @error('name')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                              </div>
-                              <div class="form-group mt-2">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ $user->email }}" name="email">
-                                @error('email')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                              </div>
-                              <div class="form-group mt-2">
-                                <label for="password">Senha</label>
-                                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-                                @error('password')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                              </div>
-                          </div>
-                          <div class="card-footer">
-                            <button type="submit" class="btn btn-primary mt-2">Salvar</button>
-                          </div>
-                      </div>
-                  </form>
+                @include('users.parts.update')
             </div>
             <div class="col-lg-6 col-12">
-                <form action="{{ route('users.perfil', $user->id) }}" method="POST">
-                    @csrf
-                    <div class="card card-primary">
-                        <div class="card-header">
-                          <h3 class="card-title">Dados do Perfil</h3>
-                        </div>
-                          <div class="card-body">
-                            <div class="form-group mt-2">
-                                <label for="type">Tipo</label>
-                                <input text="text" class="form-control @error('type') is-invalid @enderror" value="{{ $user->perfil->type }}" id="type" name="type">
-                                @error('type')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                              </div>
-                              <div class="form-group mt-2">
-                                  <label for="endereco">Endereço</label>
-                                  <input text="text" class="form-control @error('endereco') is-invalid @enderror" value="{{ $user->perfil->endereco }}" id="endereco" name="endereco">
-                                  @error('endereco')
-                                      <span class="text-danger">{{ $message }}</span>
-                                  @enderror
-                            </div>
-                          </div>
-                          <div class="card-footer">
-                            <button type="submit" class="btn btn-primary mt-2">Salvar</button>
-                          </div>
-                    </div>
-                </form>
+                @include('users.parts.roles')
+            </div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-lg-6 col-12">
+                @include('users.parts.perfil')
+            </div>
+            <div class="col-lg-6 col-12">
+                @include('users.parts.interests')
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ Route::middleware('auth')->group(function () {
         Route::get('/users/{id}', 'show')->name('users.show');
         Route::get('/users/{id}/edit', 'edit')->name('users.edit');
         Route::put('/users/{id}', 'update')->name('users.update');
-        Route::post('/users/{id}', 'perfil')->name('users.perfil');
+        Route::post('/users/{id}/perfil', 'perfil')->name('users.perfil');
+        Route::post('/users/{id}/interests', 'interests')->name('users.interests');
+        Route::post('/users/{id}/roles', 'roles')->name('users.roles');
         Route::delete('/users/{id}', 'destroy')->name('users.destroy');
     });
 });
